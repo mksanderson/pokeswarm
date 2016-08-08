ff5774f3b7a33255ca46a11b803db286cfa1ffe1
DESKTOP-GGKL93B\matthewsanderson <matthew@sandersons.id.au>
2016-08-08 21:11:17 +1000

    * Smoother loading experience

--- source/services/MapService.ts
+++ source/services/MapService.ts
@@ -155,6 +155,8 @@ namespace Application {
 			this.instance = new google.maps.Map(this.dom, {
 				center: new google.maps.LatLng(lat, lng),
 				disableDefaultUI: true,
+				maxZoom: 16,
+				minZoom: 14,
 				styles: [{ "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{ "color": "#444444" }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2f2f2" }] }, { "featureType": "poi", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": 45 }] }, { "featureType": "road.highway", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#46bcec" }, { "visibility": "on" }] }],
 				zoom: zoom
 			});

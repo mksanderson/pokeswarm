1211d2d333f35af62962f6df614961a205e014ce
DESKTOP-GGKL93B\matthewsanderson <matthew@sandersons.id.au>
2016-08-12 00:50:43 +1000

    * Major change to UX * Map/Form/Page routes * Begun creating core function tasks

--- source/controllers/ApplicationController.ts
+++ source/controllers/ApplicationController.ts
@@ -7,15 +7,41 @@ namespace Application {
 	 */
 	class ApplicationController {
 		static $inject = [
-			'FirebaseService'
+			'FirebaseService',
+			'$location',
+			'$window'
 		];
 
 		public data: any;
 
 		constructor(
-			private FirebaseService: FirebaseService
+			private FirebaseService: FirebaseService,
+			private LocationService: ng.ILocationService,
+			private WindowService: ng.IWindowService
 		) {
 			FirebaseService.configure();
+		}
+
+		/**
+		 * Reload the entire map to check for updates
+		 */
+		reload(): void {
+			this.WindowService.location.reload();
+		}
+
+		/**
+		 * Check that the current path matches the location path
+		 * 
+		 * @param {string} path (description)
+		 * @returns {boolean} (description)
+		 */
+		currentRoute(path: string): boolean{
+			if(path == this.LocationService.path()){
+				return true;
+			}
+			else{
+				return false;
+			}
 		}
 	}
 
---
+++ source/controllers/PageController.ts
@@ -0,0 +1,20 @@
+namespace Application {
+	/**
+	 * Core controller for content pages
+	 * 
+	 * @class PageController
+	 */
+	class PageController {
+		static $inject = [
+		];
+
+		constructor(
+		) {
+		
+		}
+	}
+
+	angular
+		.module('Client')
+		.controller('PageController', PageController);
+}
